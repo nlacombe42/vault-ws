58aca9d3c154eef8b917ee6211be48bf8002f8f1
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2017-08-11 00:02:48 -0400

    added /api in front of the api

---
+++ vault-api/src/main/java/net/nlacombe/vault/vaultws/api/meta/VaultWsPathConstants.java
@@ -0,0 +1,8 @@
+package net.nlacombe.vault.vaultws.api.meta;
+
+public class VaultWsPathConstants
+{
+	public static final String API_PATH_PREFIX = "/api";
+
+	public static final String V1_URL_PATH = "/v1";
+}
--- vault-api/src/main/java/net/nlacombe/vault/vaultws/api/webservice/AccountWebService.java
+++ vault-api/src/main/java/net/nlacombe/vault/vaultws/api/webservice/AccountWebService.java
@@ -1,6 +1,7 @@
 package net.nlacombe.vault.vaultws.api.webservice;
 
 import net.nlacombe.vault.vaultws.api.dto.Account;
+import net.nlacombe.vault.vaultws.api.meta.VaultWsPathConstants;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
 import javax.ws.rs.Consumes;
@@ -11,8 +12,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@FeignClient(serviceId = "vault-ws")
-@Path("accounts")
+@FeignClient(serviceId = "vault-ws", path = VaultWsPathConstants.API_PATH_PREFIX)
+@Path(VaultWsPathConstants.V1_URL_PATH + "/accounts")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface AccountWebService
