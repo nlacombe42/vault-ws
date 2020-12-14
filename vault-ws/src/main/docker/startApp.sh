32e96996c146d8f111919878a791af3dfaa9901c
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2020-12-13 20:05:23 -0500

    updated cpu/ram requests/limits

--- vault-ws/src/main/docker/startApp.sh
+++ vault-ws/src/main/docker/startApp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=prod -Dserver.port=${PORT} -Xms100m -Xmx200m -jar /app.jar
+java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=prod -Dserver.port=${PORT} -Xms100m -Xmx280m -jar /app.jar
